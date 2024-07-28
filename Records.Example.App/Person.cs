93c3d7b993ce37101624441f6cc87dbe1240a427
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-07-28 17:25:34 +0200

    feat: add validation to record.

--- Records.Example.App/Person.cs
+++ Records.Example.App/Person.cs
@@ -1,3 +1,18 @@
 ﻿namespace Records.Example.App.Test;
 public record Person(string Nombre, string Apeliido,
-    string Direccion, int Edad, string Sexo);
+    string Direccion, int Edad, string Sexo)
+{
+    private string _nombre = EsNombreValido(Nombre);
+    public string Nombre
+    {
+        get => _nombre;
+        init => _nombre = EsNombreValido(value);
+    }
+
+    private static string EsNombreValido(string nombre)
+    {
+        if(nombre.Length > 10)
+            throw new Exception();
+        return nombre;
+    }
+}
